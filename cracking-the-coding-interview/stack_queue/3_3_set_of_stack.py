69289847ea338616e04bbccffd8c3f5244d788c9
gabbr <gabbr@skbkontur.ru>
2019-08-17 12:54:09 +0300

    Set of Stacks done

--- cracking-the-coding-interview/stack_queue/3_3_set_of_stack.py
+++ cracking-the-coding-interview/stack_queue/3_3_set_of_stack.py
@@ -1,4 +1,5 @@
 from LinkedList import LinkedList, Node
+from tabulate import tabulate
 
 class Stack():
     def __init__(self, capacity=4):
@@ -42,12 +43,13 @@ class SetOfStacks():
         for i in range(n):
             self.stacks.append(Stack(capacity))
     
-    def push(value):
+    def push(self, value):
         for i in range(self.size):
             if not self.stacks[i].is_full():
                 self.stacks[i].push(value)
                 return
-            raise ValueError('Set of stacks is full')
+
+        raise ValueError('Set of stacks is full')
             
     def pop(self):
         for i in range(self.size-1, 0, -1):
@@ -60,3 +62,33 @@ class SetOfStacks():
             if not self.stacks[i].is_empty():
                 return self.stacks[i].peek()
         raise ValueError('Set of stacks is empty')
+
+    def pop_at(self, index):
+        if index >= self.size or index < 0:
+            raise ValueError('Index out of range')
+        
+        return self.stacks[index].pop()
+
+    def __str__(self):
+        temp = {}
+        for i in range(self.size):
+            if not self.stacks[i].is_empty():
+                temp[i] = list(self.stacks[i].list_)[::-1]
+
+        return str(temp)
+
+if __name__ == "__main__":
+    ss = SetOfStacks()
+    for i in range(20):
+        ss.push(i)
+
+    print(ss)
+
+    ss.pop()
+    ss.pop()
+    ss.pop()
+    ss.pop_at(0)
+    ss.pop_at(1)
+    ss.pop_at(1)
+    ss.push(999)
+    print(ss)
