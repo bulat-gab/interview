7d324be526ee41134c94add41c6d92ff774971ac
gabbr <gabbr@skbkontur.ru>
2019-11-12 09:29:52 +0300

    Rename tests

--- data_structures_algorithms/tree/tests.py
+++ data_structures_algorithms/tree/tests.py
@@ -4,12 +4,15 @@ from bst import BST, Node
 class BinarySearchTreeTests(unittest.TestCase):
     def test_get(self):
         bst = self._generate_tree()
-        actual = bst.get(1)
-        self.assertEqual(actual.value, '1')
+
+        self.assertEqual(bst.get(1).value, '1')
+        self.assertEqual(bst.get(8).value, '8')
+        self.assertEqual(bst.get(50).value, '50')
+        self.assertIsNone(bst.get(100))
     
     def _generate_tree(self):
         n1 = Node(1, '1')
-        n2 = Node(2, '2')
+        n2 = Node(8, '8')
         n3 = Node(5, '5', n1, n2)
         
         n4 = Node(50, '50')
