a74a3d686ed474315f7aa0534cb346ad5dc9797b
bulat-gab <gabdrakhmanov.br@gmail.com>
2025-01-24 15:58:15 +0300

    Add LRU cache

--- csharp/Tasks/Implementation/MyLRUCache/LRUCache.cs
+++ csharp/Tasks/Implementation/MyLRUCache/LRUCache.cs
@@ -10,61 +10,117 @@ public interface ILruCache<in TK, TV> // TODO: check if I need 'in'
 {
     TV Get(TK key);
     void Put(TK key, TV val);
+    bool Contains(TK key);
 }
 
-internal class LruCache<TK, TV>(int capacity) : ILruCache<TK, TV>
+internal class LruNode<TK, TV>(TK key, TV val)
 {
-    private readonly Dictionary<TK, LinkedListNode<TV>> _dict = [];
-    private readonly LinkedList<TV> _list = [];
+    public TK Key { get; set; } = key;
+    public TV Val { get; set; } = val;
+
+    public LruNode<TK, TV> Previous { get; set; }
+    public LruNode<TK, TV> Next { get; set; }
+
+    public static LruNode<TK, TV> CreateDummy() => new LruNode<TK, TV>(default, default);
+
+    public void Invalidate()
+    {
+        Key = default;
+        Val = default;
+        Previous = null;
+        Next = null;
+    }
+}
+public class LruCache<TK, TV> : ILruCache<TK, TV>
+{
+    private readonly Dictionary<TK, LruNode<TK, TV>> _dict = [];
+    
+    private LruNode<TK,TV> _head;
+    private LruNode<TK,TV> _tail;
+    
+    private readonly int _capacity;
+    
+    public int Count => _dict.Count;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="capacity"></param>
+    /// <exception cref="ArgumentException"></exception>
+    public LruCache(int capacity)
+    {
+        if (capacity < 2)
+        {
+            throw new ArgumentException($"Capacity must be at least 2 but was {capacity}");
+        }
+
+        _head = LruNode<TK, TV>.CreateDummy();
+        _tail = LruNode<TK,TV>.CreateDummy();
+        _head.Next = _tail;
+        _tail.Previous = _head;
+        
+        _capacity = capacity;
+    }
 
     public TV Get(TK key)
     {
-        var exists = _dict.TryGetValue(key, out var node);
+        var exists = _dict.TryGetValue(key, out LruNode<TK, TV> existingNode);
         if (!exists)
         {
             return default;
         }
 
-
-        throw new NotImplementedException();
+        AddFirst(existingNode);
+        return existingNode.Val;
     }
 
     public void Put(TK key, TV val)
     {
-        var exists = _dict.TryGetValue(key, out LinkedListNode<TV> existingNode);
-        if (!exists)
+        var exists = _dict.TryGetValue(key, out var existingNode);
+        if (exists)
         {
-            if (_list.Count < capacity)
-            {
-                var newNode = new LinkedListNode<TV>(val);
-                _dict[key] = newNode;
-                _list.AddFirst(newNode);
-                return;
-            }
-
-
-            _list.RemoveLast(); // TODO: Delete last from dictionary
-
-             var newNode2 = new LinkedListNode<TV>(val);
-            _dict[key] = newNode2;
-            _list.AddFirst(newNode2);
+            existingNode.Val = val;
+            AddFirst(existingNode);
             return;
         }
 
+        var newNode = new LruNode<TK, TV>(key, val);
+        _dict[key] = newNode;
+        AddFirst(newNode);
 
-        existingNode.Value = val;
-        var previousNode = existingNode.Previous;
+        if (Count <= _capacity)
+            return;
         
-        if (previousNode != null)
+        var keyToDelete = _tail.Previous.Key;
+        if (!_dict.Remove(keyToDelete, out var nodeToDelete))
         {
-            _list.AddFirst(existingNode); //
-            _list.Remove(existingNode);
+            // Should not happen.
+            throw new Exception($"Fatal error. Dictionary and Linked list are not synchronized.");
         }
 
+        _tail.Previous = nodeToDelete.Previous;
+        nodeToDelete.Previous.Next = _tail;
+        nodeToDelete.Invalidate();
+    }
+    
+    public bool Contains(TK key) => _dict.ContainsKey(key);
 
-
-
-
-            throw new NotImplementedException();
+    private void AddFirst(LruNode<TK, TV> node)
+    {
+        // Removes node from current position in Linked List
+        var prev = node.Previous;
+        var next = node.Next;
+        
+        if (prev != null)
+            prev.Next = next;
+        if (next != null)    
+            next.Previous = prev;
+            
+        // move to beginning of Linked List 
+        var firstNode = _head.Next;
+        node.Next = firstNode;
+        firstNode.Previous = node;
+        node.Previous = _head;
+        _head.Next = node;
     }
 }
--- csharp/Tasks/Implementation/MyLRUCache/LRUCacheTest.cs
+++ csharp/Tasks/Implementation/MyLRUCache/LRUCacheTest.cs
@@ -12,18 +12,128 @@ namespace Tasks.Implementation.MyLRUCache;
 internal class LRUCacheTest
 {
     private LruCache<string, int> _integerCache;
+    
+    private LruCache<string, string> _stringCache;
 
     [SetUp]
     public void SetUp()
     {
-        _integerCache = new LruCache<string, int>(10);
+        _integerCache = new LruCache<string, int>(4);
+        _stringCache = new LruCache<string, string>(4); 
     }
 
     [Test]
+    public void Contains_ShouldReturnTrue_WhenExists()
+    {
+        var cache = new LruCache<int, string>(2);
+        cache.Put(1, "X");
+
+        cache.Contains(1).Should().BeTrue();
+    }
+    
+    [Test]
+    public void Contains_ShouldReturnFalse_WhenNotExist()
+    {
+        var cache = new LruCache<int, string>(2);
+        cache.Contains(2).Should().BeFalse();
+    }
+    
+    [Test]
     public void Get_WhenEmptyCache()
     {
-        var result = _integerCache.Get("x");
-        result.Should().Be(default);
+        var cache = new LruCache<int, string>(2);
+        cache.Get(1).Should().Be(null);
+    }
+    
+    [Test]
+    public void Get_ShouldUpdateUsageOrder()
+    {
+        var cache = new LruCache<int, string>(2);
+
+        cache.Put(1, "A"); // Add first item
+        cache.Put(2, "B"); // Add second item
+
+        cache.Get(1); // Update first item usage
+        
+        cache.Put(3, "C"); // Add third item, causing eviction of the least recently used (2, "B")
+        
+        cache.Contains(2).Should().BeFalse();
+
+        cache.Contains(1).Should().BeTrue();
+        cache.Contains(3).Should().BeTrue();
+    }
+
+    [Test]
+    public void Put_ShouldEvictLeastRecentlyUsed_WhenCapacityExceeded()
+    {
+        var cache = new LruCache<int, string>(2);
+
+        cache.Put(1, "A"); // Add first item
+        cache.Put(2, "B"); // Add second item
+        cache.Put(3, "C"); // Add third item, causing eviction of the least recently used (1, "A")
+        
+        cache.Contains(1).Should().BeFalse();
+        cache.Contains(2).Should().BeTrue();
+        cache.Contains(3).Should().BeTrue();
+    }
+
+    [Test]
+    public void Put_ShouldUpdateValue_WhenKeyExists()
+    {
+        var cache = new LruCache<int, string>(2);
+
+        cache.Put(1, "A"); 
+        cache.Put(1, "X");
+        
+        cache.Get(1).Should().Be("X");
+    }
+    
+    [Test]
+    public void Put_ShouldUpdateUsageOrder()
+    {
+        var cache = new LruCache<int, string>(2);
+
+        cache.Put(1, "A"); // Add first item
+        cache.Put(2, "B"); // Add second item
+        cache.Put(1, "X"); // Update first item
+        cache.Put(3, "C"); // Add third item, causing eviction of the least recently used (2, "B")
+        
+        cache.Contains(2).Should().BeFalse();
+
+        cache.Contains(1).Should().BeTrue();
+        cache.Get(1).Should().Be("X");
+        cache.Contains(3).Should().BeTrue();
+    }
+    
+    [Test]
+    public void Put_RemovesLastItem_WhenCacheFull()
+    {
+        _stringCache = new LruCache<string, string>(4);
+        
+        _stringCache.Put("a", "1");
+        _stringCache.Put("b", "2");
+        _stringCache.Put("c", "3");
+        _stringCache.Put("d", "4");
+        _stringCache.Put("e", "5");
+        
+        var result = _stringCache.Get("a");
+        result.Should().BeNull();
+    }
+
+    [Test]
+    public void GetPut_ManyTimes()
+    {
+        var cache = new LruCache<int, string>(4);
+
+        for (var i = 0; i < 1000; i++)
+        {
+            var val = i.ToString();
+            cache.Put(i, val);
+            var result = cache.Get(i);
+            result.Should().Be(val);
+        }
+
+        cache.Get(999).Should().Be("999");
     }
 
 }
